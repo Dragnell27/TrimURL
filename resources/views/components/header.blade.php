cfb70472255a5fff40fd40f6da7d5f39e96c718d
Dragnell27 <jaduvase27@gmail.com>
2024-08-14 09:55:26 -0500

    .

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-start position-relative">
             <div class="container-logo me-5">
-                TrimURL
+                <img src="{{asset('trimurl.svg')}}" alt="">
             </div>
             <ul class="p-0 m-0 ms-3">
                 <li class="list-unstyled d-inline me-3"><a class="text-decoration-none" href="{{route('home')}}">Home</a></li>
                 <li class="list-unstyled d-inline"><a class="text-decoration-none" href="">Historial</a></li>
             </ul>
-            @auth
+            {{-- @auth
                 <div class="text-end position-absolute end-0">
                     <p class="d-inline">{{Auth::user()->name}}</p>
                     <a class="btn btn-warning" href="{{ route('logout') }}">Cerrar sesión</a>
@@ -18,7 +18,7 @@
                     <a class="btn btn-outline-light" href="{{ route('login') }}">Login</a>
                     <a class="btn btn-warning" href="{{ route('register') }}">Sign-up</a>
                 </div>
-            @endauth
+            @endauth --}}
         </div>
     </div>
 </header>
